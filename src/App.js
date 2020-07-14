b16f6cb4578dde4418cd6b808b3e2801b125eaec
Nullaferius <cafercotel@gmail.com>
2020-07-14 06:42:12 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
   const trans1 = (x, y) => `translate3d(${x / 20}px,${y / 20}px,0)`
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
-  const [loginState, setLoginState] = useState(1);
+  const [loginState, setLoginState] = useState(0);
   const [usernameState, setUsernameState] = useState("");
 
   const keyPress = (event) => {
